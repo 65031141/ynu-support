688354a620859e1307cb6b7a83fa9f219b9e760a
liudonghua123 <liudonghua123@gmail.com>
2016-12-20 16:39:55 +0800

    clean

--- content/vpn/How to apply for vpn account.md
+++
@@ -1,6 +0,0 @@
-Title: How to apply for vpn account
-Tags: vpn, apply for
-Authors: Donghua Liu
-Summary: Short description of this article
-
-This is the content of `How to apply for vpn account`
--- pelicanconf.py
+++ pelicanconf.py
@@ -43,10 +43,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('云南大学', 'http://www.ynu.edu.cn/'),
+         ('信息技术中心', 'http://www.itc.ynu.edu.cn/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
